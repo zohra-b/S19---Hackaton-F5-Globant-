e849de89dbf531a0ff3f2f2a604b4f7964af005b
diana <dialomt@gmail.com>
2024-02-21 21:06:55 +0100

    footer end

--- tareaya/src/components/footer/Footer.jsx
+++ tareaya/src/components/footer/Footer.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import './footer.css';
+import Facebook from '../images/Facebook.png';
+import Instagram from '../images/Instagram.png';
+import LinkedIn from '../images/LinkedIn.png';
+import YouTube from '../images/YouTube.png';
 
 const Footer = () => {
   return (
@@ -7,31 +11,31 @@ const Footer = () => {
       <div className="column">
         <h2 className="title">Explora</h2>
         <ul className="list">
-          <li>Hazte tasker</li>
-          <li>Servicios por localización</li>
-          <li>Servicios por categoría</li>
-          <li>Usuario premiun</li>
-          <li>Todos los servicios</li>
-          <li>Ayuda</li>
+          <li><a href="#">Hazte tasker</a></li>
+          <li><a href="#">Servicios por localización</a></li>
+          <li><a href="#">Servicios por categoría</a></li>
+          <li><a href="#">Usuario premiun</a></li>
+          <li><a href="#">Todos los servicios</a></li>
+          <li><a href="#">Ayuda</a></li>
         </ul>
       </div>
       <div className="column">
         <h2 className="title">Sobre nosotros</h2>
         <ul className="list">
-          <li>Quiénes somos</li>
-          <li>Noticias</li>
-          <li>Condiciones & Privacidad</li>
-          <li>Contacta</li>
-          <li>Prensa</li>
+          <li><a href="#">Quiénes somos</a></li>
+          <li><a href="#">Noticias</a></li>
+          <li><a href="#">Condiciones & Privacidad</a></li>
+          <li><a href="#">Contacta</a></li>
+          <li><a href="#">Prensa</a></li>
         </ul>
       </div>
       <div className="socials">
         <p className="socials-text">¿Aburrido? ¡Nuestras redes sociales son la solución!</p>
         <div className="icons">
-            <img src="../images/Facebook.png" alt="Facebook" />
-            <img src="../images/Instagram.png" alt="Instagram" />
-            <img src="../images/LinkedIn.png" alt="LinkedIn" />
-            <img src="../images/YouTube.png" alt="YouTube" />
+            <a href="#"><img src={Facebook} alt="Facebook" /></a>
+            <a href="#"><img src={Instagram} alt="Instagram" /></a>
+            <a href="#"><img src={LinkedIn} alt="LinkedIn" /></a>
+            <a href="#"><img src={YouTube} alt="YouTube" /></a>
         </div>
       </div>
       <div className="copyright">
