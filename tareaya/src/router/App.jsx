d9a6dbb2c7e54b30ddb08fc4bd6c6bbfc3edf5c9
lolamindi <laura_mday@yahoo.co.uk>
2024-02-22 08:59:21 +0100

    merge changes

--- tareaya/src/router/App.jsx
+++ tareaya/src/router/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import Header from "../components/header/Header";
+// import { useState } from 'react'
 import './App.css'
+import Header from "../components/header/Header";
 import Footer from '../components/footer/Footer';
 
 function App() {
     return (
       <div>
-         <Header />
+        <Header />
         <Footer />
       </div>
     );
