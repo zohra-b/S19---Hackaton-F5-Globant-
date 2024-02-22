425e6a9f340c6a19bf801de0b0617c94835f43df
dmnchmr <marformaciio@gmail.com>
2024-02-22 12:50:31 +0100

    Creation of the navbar, html structure and its css styling

--- tareaya/src/components/header/Header.jsx
+++ tareaya/src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import "./header.css";
 
 const Header = () => {
   return (
+  <>
   <header className="header--container">
     <div className="header--LogoTitle--container">
         <img src="src/components/images/LogoArbol.png"/>
@@ -15,6 +16,13 @@ const Header = () => {
         </ul>
     </div>
   </header>
+  <nav className="nav--container">
+    <ul className="nav--list">
+      <li className="nav--tasks"><img src="src/components/images/icon_search_task.png"/> BUSCAR TAREA</li>
+      <li className="nav--tasks"><img src="src/components/images/icon_offer_task.png"/> OFRECER TAREA</li>
+    </ul>
+  </nav>
+  </>
 )};
 
 export default Header;
